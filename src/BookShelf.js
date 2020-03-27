354fb311dc25414509955c3ebd9c80877518e970
Paul Spende <paul.spende@nexenio.com>
2020-03-27 19:05:26 +0100

    refactor: BookShelf

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { Book } from './Book';
 
-export const BookShelf = ({ title, books, updateBook }) => {
-  return (
-    <div className="bookshelf">
-      <h2 className="bookshelf-title">{title}</h2>
-      <div className="bookshelf-books">
-        <ol className="books-grid">
-          {books.map(book => (
-            <Book book={book} key={book.id} updateBook={updateBook} />
-          ))}
-        </ol>
-      </div>
+export const BookShelf = ({ title, books, updateBook }) => (
+  <div className="bookshelf">
+    <h2 className="bookshelf-title">{title}</h2>
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+        {books.map(book => (
+          <Book book={book} key={book.id} updateBook={updateBook} />
+        ))}
+      </ol>
     </div>
-  );
-};
+  </div>
+);
