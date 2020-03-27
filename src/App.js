d8b3eab53d525d5c2fc37d19ad86bedd5c8d23d7
Paul Spende <paul.spende@nexenio.com>
2020-03-27 22:20:54 +0100

    feat/bug: new more reliable logic

--- src/App.js
+++ src/App.js
@@ -7,51 +7,35 @@ import './App.css';
 
 class BooksApp extends Component {
   state = {
-    currentlyReading: [],
-    wantToRead: [],
-    read: []
+    books: []
   };
 
   async componentDidMount() {
-    const books = await getAll();
-    const currentlyReading = books.filter(
-      book => book.shelf === 'currentlyReading'
-    );
-    const wantToRead = books.filter(book => book.shelf === 'wantToRead');
-    const read = books.filter(book => book.shelf === 'read');
-    this.setState({ currentlyReading, wantToRead, read });
+    getAll().then(books => this.setState({ books }));
   }
 
   updateBook = async (bookId, oldShelf, newShelf) => {
-    const updatedOld = this.state[oldShelf].filter(book => book.id !== bookId);
     const updatedBook = await get(bookId);
-    if (newShelf !== 'none') {
-      this.setState(oldState => ({
-        [newShelf]: [...oldState[newShelf], updatedBook]
-      }));
-    }
-    this.setState({
-      [oldShelf]: updatedOld
+    this.setState(({ books }) => {
+      const restBooks = this.state.books.filter(book => book.id !== bookId);
+      if (updatedBook.shelf === 'none') return { books: restBooks };
+      return { books: [...restBooks, updatedBook] };
     });
   };
 
   render() {
-    const { currentlyReading, wantToRead, read } = this.state;
+    const { books } = this.state;
     return (
       <div className="app">
         <Route
           exact
           path="/"
-          render={() => (
-            <MyReads
-              currentlyReading={currentlyReading}
-              wantToRead={wantToRead}
-              read={read}
-              updateBook={this.updateBook}
-            />
-          )}
+          render={() => <MyReads books={books} updateBook={this.updateBook} />}
         />
-        <Route path="/search" component={SearchBooks} />
+        <Route
+          path="/search"
+          render={() => <SearchBooks updateBook={this.updateBook} />}
+        />
       </div>
     );
   }
